03f508e55ec1cf0ef23e9e98d24409b886a571f4
Bolaji Oluwatofunmi <tofunmibolaji13@gmail.com>
2025-06-09 01:03:15 -0700

    update

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/logo.png";
 import { motion, AnimatePresence } from "framer-motion";
 
 function Navigation() {
