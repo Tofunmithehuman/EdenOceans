03a818704701796a9b64d34b69d50c45b0cbf6ed
Bolaji Oluwatofunmi <tofunmibolaji13@gmail.com>
2025-06-11 16:57:09 -0700

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -468,10 +468,8 @@ function Home() {
                         <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
                           Ocean Cruises
                         </p>
-                        <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center h-30">
-                          Luxurious cruises in destinations like the Maldives, and Fiji
-                          Islands, offering wellness activities onboard and shore excursions to pristine beaches and
-                          luxury resorts.
+                        <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center h-40">
+                          Enjoy the best of private, adults-only 5-star cruises around the world including European cruises, Asian cruises visiting Singapore, South Korea, Thailand, Malaysia, and Japan, and Caribbean cruises to popular Caribbean destinations and private island resorts.
                         </p>
                       </div>
                       <motion.img
@@ -488,11 +486,10 @@ function Home() {
                     >
                       <div className="p-3">
                         <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                          Luxury Wellness Retreats
+                          Advanced Wellness and Lifestyle Management
                         </p>
-                        <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center h-30">
-                          Experience immersive experiences in beautiful destinations like Mauritius, Barbados, and Cape Verde, blending relaxation, adventure, and
-                          healing at luxury resorts.
+                        <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center h-40">
+                          Experience the beauty and transformative power of our advanced wellness options including complete Home SPAs, specialist wellness evaluations, full makeover retreats, international telemedicine consultations, life coaching, and global wellness retreats.
                         </p>
                       </div>
                       <motion.img
@@ -530,7 +527,7 @@ function Home() {
                     >
                       <div className="p-3">
                         <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                          Exclusive Events
+                          Red Carpet Event Access
                         </p>
                         <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center h-30">
                           Book Clubs, Game Nights, Dance, Painting and Cooking Classes, and more,
@@ -828,7 +825,7 @@ function Home() {
                         <img alt="" src={person.imageUrl} className="size-16 rounded-full" />
                         <div>
                           <h3 className="text-base/7 font-semibold tracking-tight text-gray-900">{person.name}</h3>
-                          <p className="text-sm/6 font-semibold text-indigo-600">{person.role}</p>
+                          <p className="text-sm/6 font-semibold text-primary">{person.role}</p>
                         </div>
                       </motion.div>
                     </li>
@@ -838,8 +835,145 @@ function Home() {
             </motion.div>
           </section>
 
+          <section className="contact-section" ref={contactRef}>
+            <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+              <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+                <div
+                  className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+                  style={{
+                    clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
+                  }}
+                ></div>
+              </div>
+              <div className="mx-auto max-w-2xl text-center">
+                <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl bricolage-grotesque">Contact Us</h2>
+                <p className="mt-2 text-lg/8 text-gray-600 bricolage-grotesque">Get in touch with us, we’d love to hear from you!</p>
+              </div>
+              <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                  <div>
+                    <label htmlFor="first-name" className="block text-sm/6 font-semibold text-gray-900">First name</label>
+                    <div className="mt-2.5">
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        autoComplete="given-name"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label htmlFor="last-name" className="block text-sm/6 font-semibold text-gray-900">Last name</label>
+                    <div className="mt-2.5">
+                      <input
+                        type="text"
+                        name="last-name"
+                        id="last-name"
+                        autoComplete="family-name"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="Country" className="block text-sm/6 font-semibold text-gray-900">Country</label>
+                    <div className="mt-2.5">
+                      <input
+                        type="text"
+                        name="Country"
+                        id="Country"
+                        autoComplete="organization"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="email" className="block text-sm/6 font-semibold text-gray-900">Email</label>
+                    <div className="mt-2.5">
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        autoComplete="email"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary"
+                      />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">Phone number</label>
+                    <div className="mt-2.5">
+                      <div className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-primary">
+                        <div className="grid shrink-0 grid-cols-1 focus-within:relative">
+                          <select
+                            id="country"
+                            name="country"
+                            autoComplete="country"
+                            aria-label="Country"
+                            className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6"
+                          >
+                            <option>US</option>
+                            <option>CA</option>
+                            <option>EU</option>
+                            <option>UK</option>
+                            <option>NG</option>
+                            <option>GH</option>
+                            <option>KE</option>
+                            <option>ZA</option>
+                            <option>Other</option>
+                          </select>
+                          <svg
+                            className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            aria-hidden="true"
+                            data-slot="icon"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </div>
+                        <input
+                          type="text"
+                          name="phone-number"
+                          id="phone-number"
+                          className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                          placeholder="123-456-7890"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="message" className="block text-sm/6 font-semibold text-gray-900">Message</label>
+                    <div className="mt-2.5">
+                      <textarea
+                        name="message"
+                        id="message"
+                        rows="4"
+                        className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary"
+                      ></textarea>
+                    </div>
+                  </div>
 
-          <section id="contact-section" className="pt-16" ref={contactRef}>
+                </div>
+                <div className="mt-10">
+                  <button
+                    type="submit"
+                    className="block w-full rounded-md bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                  >
+                    Let's talk
+                  </button>
+                </div>
+              </form>
+            </div>
+          </section>
+
+
+
+
+          <section id="footer-section" className="pt-16">
             <div className="Footer">
               <div className="bg-primary text-white">
                 <div className="max-w-7xl mx-auto pt-16 pb-8 px-6 sm:px-10">
