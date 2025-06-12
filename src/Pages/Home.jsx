c3194daf162287d82c5e545577c75180b8d9c657
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2025-06-12 04:11:09 -0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -749,7 +749,7 @@ function Home() {
                           Royal Membership is designed for young women 21-49 who are ready to discover or enhance purposeful living,  and secure and build meaningful social and community connections to thrive and prosper, using their new strengths, gifts, skills and  talents. At EdenOceans our vision for Royal members is to stimulate self-discovery and improve skills in the area of show business, music, movie production, acting, performing arts and other non traditional career paths.                        </p>
                         <div className="mt-4 flex justify-center">
                           <motion.a
-                            className="bg-secondary text-primary font-medium py-2 px-4 rounded-lg w-full cursor-pointer"
+                            className="bg-secondary text-primary text-center font-medium py-2 px-4 rounded-lg w-full cursor-pointer"
                             variants={buttonVariants}
                             whileHover="hover"
                             whileTap="tap"
